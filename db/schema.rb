6fa8c3994c176fad8596c2b5095311068d10c337
ya.agarkov <ya.agarkov@i-sys.ru>
2016-01-24 02:37:27 +0500

    add name fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119191958) do
+ActiveRecord::Schema.define(version: 20160123212340) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20160119191958) do
     t.string   "password",   limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "first_name", limit: 255
+    t.string   "last_name",  limit: 255
   end
 
 end
