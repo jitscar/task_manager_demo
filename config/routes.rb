5fd51dc83e4bba51349e547702ad8911ae844038
ya.agarkov <ya.agarkov@i-sys.ru>
2016-01-24 16:10:57 +0500

    add registration, selecting from users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :users
   resources :tasks
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'tasks#index'
 
   # Example of regular route:
