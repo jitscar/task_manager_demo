f8ff6c4a0c60a14e0d0bd5258ade7faafd5f3f29
ya.agarkov <ya.agarkov@i-sys.ru>
2016-01-24 05:17:12 +0500

    add aasm state to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,24 @@
 class Task < ActiveRecord::Base
+  STATES = %w(new started finished)
+
   belongs_to :user
 
   validates :name, presence: true
+  validates :state, presence: true, inclusion: { in: STATES }
+
+  include AASM
+
+  aasm column: :state do
+    state :new, initial: true
+    state :started
+    state :finished
+
+    event :start do
+      transitions from: :new, to: :started
+    end
+
+    event :finish do
+      transitions from: [:new, :started], to: :finished
+    end
+  end
 end
